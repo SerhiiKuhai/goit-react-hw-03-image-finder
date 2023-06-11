4412bc0b02dc7fd2091292256f0dbced6566cd29
SerhiiKuhai <sergk30@gmail.com>
2023-06-11 22:29:06 +0300

    s1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,20 @@
 import { Component } from 'react';
 
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+export class App extends Component {
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        React homework templatehmnnbvv
+      </div>
+    );
+  }
+}
